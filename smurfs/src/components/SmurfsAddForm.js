02d3d3fa31f2e1ee4bd6e1581b2ba3c29a5252da
Adnan <adnan.chowdhury@outlook.com>
2019-08-16 11:36:23 -0500

    Form built for adding smurfs

---
+++ smurfs/src/components/SmurfsAddForm.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const SmurfsAddForm = () => {
+  const [smurf, setSmurf] = useState({
+    name: "",
+    age: "",
+    height: ""
+  });
+
+  const handleChanges = e => {
+    setSmurf({ ...smurf, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(smurf);
+    setSmurf({
+      name: "",
+      age: "",
+      height: ""
+    });
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={smurf.name}
+        name="name"
+        placeholder="Name for Smurf"
+        onChange={handleChanges}
+      />
+      <input
+        type="text"
+        value={smurf.age}
+        name="age"
+        placeholder="Age for Smurf"
+        onChange={handleChanges}
+      />
+      <input
+        type="text"
+        value={smurf.height}
+        name="height"
+        placeholder="Height for Smurf"
+        onChange={handleChanges}
+      />
+      <button>Add</button>
+    </form>
+  );
+};
+
+export default SmurfsAddForm;
