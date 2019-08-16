3caf4fe1cbbfbe6e52b89dc534ec6fc76934dc6b
Adnan <adnan.chowdhury@outlook.com>
2019-08-16 10:42:38 -0500

    Store initialized, parsing error with initial state, fixed by putting colon

--- smurfs/src/reducers/smurfReducer.js
+++ smurfs/src/reducers/smurfReducer.js
@@ -1,11 +1,11 @@
 const initialState = {
-    smurfs=[],
-    error:""
-}
+  smurfs: [],
+  error: ""
+};
 
-export const reducer = (state=initialState, action) =>{
-    switch(action.type){
-        default:
-            return state;
-    }
-}
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
